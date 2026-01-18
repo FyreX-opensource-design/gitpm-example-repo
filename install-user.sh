3ce4da3d0dae62f7eb78119f4150bfa2ec78de42
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2026-01-18 17:25:47 -0600

    Update install-user.sh

--- install-user.sh
+++ install-user.sh
@@ -1,1 +1,2 @@
-ln -s ./proccessctl.sh ~/.local/bin/proccessctl
+chmod +x ~/.local/share/apps/proccessctl/proccessctl.sh
+ln -s ~/.local/share/apps/proccessctl/proccessctl.sh ~/.local/bin/proccessctl
